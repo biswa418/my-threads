80bd2c839ded02f406fcf631b36bf70036a73018
Biswajeet Sutar <biswajeetsutar418@gmail.com>
2023-10-08 22:40:46 +0530

    add interactions and images with padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,25 @@
 import { LeftSidebar } from "@/components"
-import { BsDot } from "react-icons/bs"
+import { BsChat, BsDot } from "react-icons/bs"
+import { AiOutlineHeart, AiOutlineRetweet } from 'react-icons/ai'
+import { IoShareOutline, IoStatsChart } from 'react-icons/io5'
+
+const icons = [
+  {
+    icon: BsChat
+  },
+  {
+    icon: AiOutlineRetweet
+  },
+  {
+    icon: AiOutlineHeart
+  },
+  {
+    icon: IoStatsChart
+  },
+  {
+    icon: IoShareOutline
+  }
+]
 
 const Home = () => {
   return (
@@ -8,8 +28,8 @@ const Home = () => {
         {/* left sidebar */}
         <LeftSidebar />
 
-        <main className="ml-72 flex w-[600px] h-full min-h-screen py-6 flex-col border-l border-r border-slate-900">
-          <h1 className="text-xl font-bold px-6 mb-6">Home</h1>
+        <main className="ml-72 flex w-[600px] h-full min-h-screen flex-col border-l border-r border-slate-900">
+          <h1 className="text-xl font-bold p-5 backdrop-blur z-10 bg-black/10 sticky top-0">Home</h1>
 
           <div className="border-t border-b px-6 flex items-stretch py-4 space-x-2 border-slate-900 relative">
             <div className="w-10 h-10 bg-slate-400 rounded-full flex-none">
@@ -37,28 +57,43 @@ const Home = () => {
           {/* posts */}
           <div className="flex flex-col">
             {
-              Array(6).fill(0).map((_, i) => {
+              Array(10).fill(0).map((_, i) => {
                 return (
-                  <div key={i} className="border-t border-b border-slate-900 flex space-x-4">
+                  <div key={i} className="border-b p-4 border-slate-900 flex space-x-4">
                     <div>
                       <div className="w-10 h-10 rounded-full bg-slate-300"></div>
                     </div>
 
                     <div className="flex flex-col">
                       <div className="flex items-center space-x-1">
-                        <div>Biswajeet</div>
-                        <div>@biswajeet</div>
-                        <div>
+                        <div className="font-bold">Biswajeet</div>
+                        <div className="text-slate-600">@biswajeet</div>
+                        <div className="text-slate-600">
                           <BsDot />
                         </div>
-                        <div>1 hour ago</div>
+                        <div className="text-slate-600">1 hour ago</div>
                       </div>
 
-                      <div className="text-sm">
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos nesciunt, nemo tempora harum sint fugit! Veritatis, nam unde explicabo porro, harum quaerat asperiores dolore natus consectetur est magnam nemo. Minima.
+                      {/* captions */}
+                      <div className="text-sm mb-3 mt-1">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos nesciunt, nemo tempora harum sint fugit! Veritatis, nam unde explicabo porro, harum quaerat asperiores dolore natus consectetur est magnam nemo.
                       </div>
-                      <div>
 
+                      {/* photo */}
+                      <div className="bg-slate-400 aspect-square w-full h-96 rounded-lg">
+                      </div>
+
+                      {/* interactions */}
+                      <div className="flex items-center space-x-12 mt-2 w-full">
+                        {
+                          icons.map((icon) => {
+                            return (
+                              <div className="rounded-full hover:bg-white/10 transition duration-200 p-3 cursor-pointer">
+                                <icon.icon />
+                              </div>
+                            )
+                          })
+                        }
                       </div>
                     </div>
                   </div>
