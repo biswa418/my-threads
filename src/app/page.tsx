e7801b1f1b4b2705d886041612b71569780ffcb8
Biswajeet Sutar <biswajeetsutar418@gmail.com>
2023-10-11 22:08:34 +0530

    fronend UI skeleton done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,9 +42,19 @@ const Home = () => {
             <div>
               {
                 Array(5).fill(0).map((_, i) => (
-                  <div key={i} className="hover:bg-white/10 hover:cursor-pointer p-4 last:rounded-b-xl transition duration-200">
-                    <div className="font-bold">Trending Item {i + 1}</div>
-                    <div className="text-xs text-neutral-500">30.2k</div>
+                  <div key={i} className="hover:bg-white/10 justify-between text-sm hover:cursor-pointer p-4 flex items-center last:rounded-b-xl transition duration-200">
+                    <div className="flex items-center space-x-2">
+                      <div className="w-10 h-10 bg-neutral-600 rounded-full"></div>
+                      <div className="flex flex-col">
+                        <div className="">Other user</div>
+                        <div>@otheruser12</div>
+                      </div>
+                    </div>
+                    <div>
+                      <button className="rounded-full px-4 py-2 bg-white hover:bg-white/80 hover:text-neutral-700 text-xs transition duration-200 font-semibold text-neutral-950">
+                        Follow
+                      </button>
+                    </div>
                   </div>
                 ))
               }
