47ab15f4cee2a2aa081a22be51a95e7ba2752814
Biswajeet Sutar <biswajeetsutar418@gmail.com>
2023-11-23 19:09:49 +0530

    auth modal add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import { LeftSidebar, MainComponent, RightSection } from "@/components";
+import AuthModal from "@/components/ui/AuthModal";
 
-const Home = () => {
+const Home = async () => {
   return (
-    <div className="w-full h-full flex text-white justify-center items-center">
-      <div className="md:max-w-screen-xl max-w-screen-lg w-full h-full flex relative">
-        <LeftSidebar />
-        <MainComponent />
-        <RightSection />
+    <>
+      <AuthModal />
+      <div className="w-full h-full flex text-white justify-center items-center">
+        <div className="md:max-w-screen-xl max-w-screen-lg w-full h-full flex relative">
+          <LeftSidebar />
+          <MainComponent />
+          <RightSection />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
